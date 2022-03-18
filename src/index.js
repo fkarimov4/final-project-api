6c919e5a2e3980acfcd08f93fd8c209a55e38a51
Farrukh Karimov <fkarimov4@gmail.com>
2022-03-18 09:58:46 -0400

    added JWT verification to API routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const admin = require('firebase-admin');
 const cors = require("cors");
-const { getJobs, getFilteredJobs, getRecentJobs, getSingleJob } = require("./jobs");
+const {
+  getJobs,
+  getFilteredJobs,
+  getRecentJobs,
+  getSingleJob,
+} = require("./jobs");
 const { addUser, updateUser, getUser } = require("./users");
 
 const port = process.env.PORT || 3000;
@@ -10,14 +16,30 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const withAuthorization = async (req, res, next) => {
+  const jwt = req.headers.authorization;
+  try {
+    const id = await admin.auth().verifyIdToken(jwt);
+    res.locals.userId = id.uid;
+    res.send({
+      success: true,
+      message: "User authorized with token"
+    })
+  } catch {
+    res.status(403).send("Unauthorized");
+    return;
+  }
+  next();
+};
+
 app.get("/jobs", getJobs);
 app.post("/jobs/filtered", getFilteredJobs);
 app.get("/jobs/recent", getRecentJobs);
 app.get("/jobs/:jobId", getSingleJob);
 
-app.post("/users/add", addUser)
-app.get("/users/:userId", getUser)
-app.patch("/users/:userId", updateUser)
+app.post("/users/add", withAuthorization, addUser);
+app.get("/users/:userId", getUser);
+app.patch("/users/:userId", withAuthorization, updateUser);
 
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
